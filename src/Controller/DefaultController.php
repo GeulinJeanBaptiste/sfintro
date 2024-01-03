a3d665c1c3b62ae40b76651ccc5ceda8bb1c4a41
Geulin Jean-Baptiste <jeanbaptiste.geulin@gmail.com>
2024-01-03 16:47:51 +0100

    Ajout fonction number

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,11 +3,19 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
 
 class DefaultController
 {
+    #[Route('/')]
     public function index(): Response
     {
         return new Response('Hello World!');
+    }
+    #[Route('/number')]
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+        return new Response($number);
     }
 }
