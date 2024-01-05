e9cd14a99d65b24036355fb37ed8f7f14173f0ff
Geulin Jean-Baptiste <jeanbaptiste.geulin@gmail.com>
2024-01-05 15:22:27 +0100

    Ajout d'une navbar

--- src/Controller/EmployeeController.php
+++ src/Controller/EmployeeController.php
@@ -11,8 +11,26 @@ class EmployeeController extends AbstractController
     #[Route('/employee', name: 'app_employee')]
     public function index(): Response
     {
+        $employees = [
+
+            [
+                'Name' => 'Jane Doe',
+                'Age' => 34,
+                'Email' => 'johndoe@me.com',
+            ],
+            [
+                'Name' => 'john Doe2',
+                'Age' => 45,
+                'Email' => 'johndoe2@me.com',
+            ],
+            [
+                'Name' => 'john Doe3',
+                'Age' => 57,
+                'Email' => 'johndoe3@me.com',
+            ]
+        ];
         return $this->render('employee/index.html.twig', [
-            'controller_name' => 'EmployeeController',
+            'employees' => $employees, 'title' => 'Liste des employés',
         ]);
     }
 }
--- src/Controller/contactController.php
+++ src/Controller/contactController.php
@@ -3,15 +3,17 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 
-class ContactController
+class ContactController extends AbstractController
 {
     #[Route('/contact', 'contact')]
-    public function contact(): Response
+    public function index(): Response
     {
         $contact = random_int(0, 50);
-        return new Response('<html><body>QI de BEN CON : ' . $contact . '</body></html>');
+        // return new Response('<html><body>QI de BEN CON : ' . $contact . '</body></html>');
+        return $this->render('contact/index.html.twig');
     }
 }
